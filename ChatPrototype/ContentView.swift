d3177ac86370a2f545c92ae70aa8dfd66e2bb08c
Tony Sharples <tonysharples22@me.com>
2024-04-08 13:10:36 +0100

    Create chat views

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -9,11 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Text("Look at me.")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 5)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Text("I'm Text!")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 5)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            
+            Text("This is pretty cool.")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 5)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Text("I know right?")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 5)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            
+            Spacer()
         }
         .padding()
     }
