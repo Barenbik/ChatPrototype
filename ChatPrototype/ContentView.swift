bc346af89400d74190e8c19003ff4f75d1770dd7
Tony Sharples <tonysharples22@me.com>
2024-04-19 16:14:53 +0100

    Create ChatBubble view

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -10,33 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(spacing: 30) {
-            Text("Look at me.")
-                .padding()
-                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 30))
-                .shadow(radius: 5)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            
-            Text("I'm Text!")
-                .padding()
-                .background(Color.teal, in: RoundedRectangle(cornerRadius: 30))
-                .shadow(radius: 5)
-                .frame(maxWidth: .infinity, alignment: .trailing)
-            
-            Text("This is pretty cool.")
-                .padding()
-                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 30))
-                .shadow(radius: 5)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            
-            Text("I know right?")
-                .padding()
-                .background(Color.teal, in: RoundedRectangle(cornerRadius: 30))
-                .shadow(radius: 5)
-                .frame(maxWidth: .infinity, alignment: .trailing)
+            ChatBubble(text: "Look at me", colour: Color.yellow, alignment: .leading)
+            ChatBubble(text: "I'm Text!", colour: Color.teal, alignment: .trailing)
+            ChatBubble(text: "This is pretty cool", colour: Color.yellow, alignment: .leading)
+            ChatBubble(text: "I know right?", colour: Color.teal, alignment: .trailing)
             
             Spacer()
         }
         .padding()
+    }
+}
+
+struct ChatBubble: View {
+    let text: String
+    let colour: Color
+    let alignment: Alignment
+    
+    var body: some View {
+        Text(text)
+            .padding()
+            .background(colour, in: RoundedRectangle(cornerRadius: 30))
+            .shadow(radius: 5)
+            .frame(maxWidth: .infinity, alignment: alignment)
     }
 }
 
